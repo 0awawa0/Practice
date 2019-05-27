4037489d906da8a6e92cf7649911d62b78a8150a
wunderleth <winderleth@gmail.com>
2019-05-27 21:16:18 +0300

    1) Eval no more! 2) Some additions and corrections

--- scripts/Calculator/main_window.py
+++ scripts/Calculator/main_window.py
@@ -1,20 +1,11 @@
+#
 import mainWindow_interface
 from PyQt5 import QtWidgets
-import math
-
-
-ctg = lambda a: round(math.sin(math.radians(a)), 3) / round(cos(math.radians(a)), 3)
-cos = lambda a: math.cos(math.radians(a))
-sin = lambda a: math.sin(math.radians(a))
-tan = lambda a: math.tan(math.radians(a))
+import math_parser
 
 
 class CalculatorApp(QtWidgets.QMainWindow,
                     mainWindow_interface.Ui_MainWindow):
-
-    OPERATORS = ["+", "-", "*", "/", "**"]
-
-    FUNCTIONS = ["sin", "cos", "tan", "ctg"]
 
     def __init__(self):
 
@@ -22,7 +13,9 @@ class CalculatorApp(QtWidgets.QMainWindow,
         self.setupUi(self)
         self.expression = ""
         self.is_result = False
+        self.mem = None
 
+        # Connect buttons to functions
         self.opparen_button.clicked.connect(lambda: self.insert_text("("))
         self.clparen_button.clicked.connect(lambda: self.insert_text(")"))
 
@@ -41,19 +34,74 @@ class CalculatorApp(QtWidgets.QMainWindow,
         self.minus_button.clicked.connect(lambda: self.insert_text("-"))
         self.mul_button.clicked.connect(lambda: self.insert_text("*"))
         self.division_button.clicked.connect(lambda: self.insert_text("/"))
-        self.pow_button.clicked.connect(lambda: self.insert_text("**"))
+        self.pow_button.clicked.connect(lambda: self.insert_text("^"))
 
         self.sin_button.clicked.connect(lambda: self.insert_text("sin"))
         self.cos_button.clicked.connect(lambda: self.insert_text("cos"))
-        self.tan_button.clicked.connect(lambda: self.insert_text("tan"))
+        self.tan_button.clicked.connect(lambda: self.insert_text("tg"))
         self.cotan_button.clicked.connect(lambda: self.insert_text("ctg"))
 
         self.dot_button.clicked.connect(lambda: self.insert_text("."))
 
+        self.del_button.clicked.connect(self.delete)
+        self.clear_button.clicked.connect(self.clear_field)
+        self.toMem_button.clicked.connect(self.to_mem)
+        self.fromMem_button.clicked.connect(self.from_mem)
+
         self.result_button.clicked.connect(self.calculate)
 
+    def clear_field(self):
+        self.result_field.clear()
+        self.expression = ""
+        self.is_result = False
+
+    def delete(self):
+        if self.is_result:
+            self.expression = ""
+            self.result_field.clear()
+
+        if self.expression != "":
+            if self.expression[-1] == " ":
+                self.expression = self.expression[:-2]
+            else:
+                self.expression = self.expression[:-1]
+            self.result_field.setText(self.expression)
+
+    def to_mem(self):
+        try:
+            self.mem = float(self.expression)
+        except Exception:
+            return
+
+    def from_mem(self):
+        if not self.mem:
+            return
+
+        if self.expression == "" or self.is_result:
+            self.expression = str(self.mem)
+            self.result_field.setText(self.expression)
+            return
+
+        try:
+            float(self.expression)
+            self.expression = str(self.mem)
+            self.result_field.setText(self.expression)
+            return
+        except ValueError:
+            lst = self.expression[-1]
+            if lst == " ":
+                self.expression += str(self.mem)
+            if lst == "(":
+                self.expression += str(self.mem) + " "
+            self.result_field.setText(self.expression)
+
     def insert_text(self, char: str):
-        if self.expression:
+        """
+        Insert number, operation or function to the final expression
+        :param char:
+        :return:
+        """
+        if self.expression != "":
             lst = self.expression[-1]
         else:
             lst = ""
@@ -61,52 +109,84 @@ class CalculatorApp(QtWidgets.QMainWindow,
         if char.isdigit():
             if lst == ")":
                 return
+
             if self.is_result:
                 self.expression = char
                 self.result_field.setText(self.expression)
                 self.is_result = False
                 return
+
             self.expression += char
             self.result_field.setText(self.expression)
             return
 
         if char == ".":
+
             if lst.isdigit():
                 if "." in self.expression.split(" ")[-1]:
                     return
+
                 else:
                     self.expression += char
                     self.result_field.setText(self.expression)
+                    return
+
             if self.expression == ".":
                 return
-        if char in CalculatorApp.FUNCTIONS:
+
+        if char in math_parser.FUNCTIONS:
             if self.is_result:
                 self.expression = f"{char}({self.expression})"
                 self.calculate()
                 return
+
             if lst.isdigit():
                 return
+
             self.expression += f"{char}("
             self.result_field.setText(self.expression)
             return
-        if char in CalculatorApp.OPERATORS:
+
+        if char in math_parser.OPERATORS:
             if lst.isdigit() or self.expression[-1] == ")":
                 self.expression += f" {char} "
                 self.result_field.setText(self.expression)
+            self.is_result = False
             return
+
         if char == "(":
             if lst.isdigit():
                 return
+
             self.expression += "("
             self.result_field.setText(self.expression)
+
         if char == ")":
             if not lst.isdigit() and lst != ")":
                 return
+
             self.expression += ")"
             self.result_field.setText(self.expression)
 
     def calculate(self):
-        result = round(eval(self.expression, globals(), locals()), 4)
+        """
+        Calculate the expression and print the result
+        :return:
+        """
+        try:
+            result = round(math_parser.calculate(self.expression), 4)
+        except SyntaxError as exception:
+            self.expression = ""
+            self.is_result = False
+            self.mem = None
+            self.result_field.setText(f"Wrong syntax! {exception}")
+            return
+        except Exception:
+            self.expression = ""
+            self.is_result = False
+            self.mem = None
+            self.result_field.setText(f"Wrong syntax!")
+            return
 
         if int(result) == result:
             result = f"{int(result)}"
--- scripts/Calculator/math_parser.py
+++ scripts/Calculator/math_parser.py
@@ -1,4 +1,216 @@
+from collections import namedtuple
+import operator
+import math
+
+# Struct for operators
+Operator = namedtuple("Operator", "prec assoc func")
+
+# Struct for fuctions
+Function = namedtuple("Function", "arg_count func")
+
+NUMBERS = "0123456789"
+
+OPERATORS = {
+    "+": Operator("1", "Left", operator.add),
+    "-": Operator("1", "Left", operator.sub),
+    "*": Operator("2", "Left", operator.mul),
+    "/": Operator("2", "Left", operator.truediv),
+    "^": Operator("3", "Right", operator.pow)
+}
+
+FUNCTIONS = {
+    "sin": Function(1, lambda x: math.sin(math.radians(x))),
+    "cos": Function(1, lambda x: math.cos(math.radians(x))),
+    "tg": Function(1, lambda x: math.tan(math.radians(x))),
+    "ctg": Function(1, lambda x: 1 / math.tan(math.radians(x))),
+    "max": Function("multiple", max)
+}
 
 
+def parse_expression(expression: str):
+    """
+    This function parses the math expression, excludes all useless characters
+    and returns list with numbers, operators, functions, parentherses and
+    separators of arguments.
+    """
+
+    if expression[-1] == ".":
+        raise SyntaxError("Expression can't end with '.'")
+
+    out = []
+    pointer = 0
+    while pointer < len(expression):
+
+        if expression[pointer] in NUMBERS:
+
+            number = ""
+            while expression[pointer] in NUMBERS or expression[pointer] == '.':
+                if expression[pointer] == '.':
+                    if '.' in number:
+                        raise SyntaxError("Two '.' in one number")
+                    else:
+                        number += '.'
+                else:
+                    number += expression[pointer]
+                pointer += 1
+                if pointer >= len(expression):
+                    break
+
+            out.append(float(number))
+            continue
+
+        if expression[pointer] in OPERATORS:
+            out.append(expression[pointer])
+            pointer += 1
+            continue
+
+        if expression[pointer] in "(),":
+            out.append(expression[pointer])
+            pointer += 1
+            continue
+
+        if expression[pointer] == " ":
+            pointer += 1
+            continue
+
+        function_name = ""
+        while expression[pointer] != "(":
+            try:
+                function_name += expression[pointer]
+            except IndexError:
+                raise SyntaxError("There must be '(' after function name")
+            pointer += 1
+
+        if function_name in FUNCTIONS:
+            out.append(function_name)
+            out.append(expression[pointer])
+            pointer += 1
+            continue
+        else:
+            raise SyntaxError(f"No such function '{function_name}'")
+
+    return out
 
 
+def shunting_yard(expression: list):
+    """
+    This function implements Shunting yard algorithm. 
+    Function needs preprocessed list from parse_expression function.
+    Function returns list of expression in reversed polish notation.
+    """
+    if isinstance(expression, str):
+        expression = parse_expression(expression)
+
+    out = []
+    stack = []
+    functions_args_counter = []
+
+    for token in expression:
+        if isinstance(token, float):
+            out.append(token)
+            continue
+
+        if token in FUNCTIONS:
+            functions_args_counter.append(0)
+            stack.append(token)
+            continue
+
+        if token == ",":
+            functions_args_counter[-1] += 1
+            while stack[-1] != "(":
+                try:
+                    out.append(stack.pop())
+                except IndexError:
+                    raise SyntaxError("Missing ',' or '('")
+            continue
+
+        if token in OPERATORS:
+            op1 = OPERATORS[token]
+            if len(stack) == 0:
+                stack.append(token)
+                continue
+            op2 = stack[-1]
+            if op2 not in OPERATORS:
+                stack.append(token)
+                continue
+            op2 = OPERATORS[op2]
+            while (op1.assoc == "Left" and op1.prec <= op2.prec or
+                   op1.assoc == "Right" and op1.prec < op2.prec) and len(stack) > 0:
+                out.append(stack.pop())
+            stack.append(token)
+            continue
+
+        if token == "(":
+            stack.append(token)
+            continue
+
+        if token == ")":
+            while stack[-1] != "(":
+                out.append(stack.pop())
+            stack.pop(len(stack)-1)
+            if stack[-1] in FUNCTIONS:
+                if FUNCTIONS[stack[-1]].arg_count == "multiple":
+                    out.append(functions_args_counter[-1] + 1)
+                out.append(stack.pop())
+                functions_args_counter.pop()
+            continue
+
+    while len(stack) > 0:
+        if stack[-1] in "()":
+            raise SyntaxError("Unclosed parentheses")
+        out.append(stack.pop(len(stack)-1))
+
+    return out
+
+
+def calculate(expression: list):
+    """
+    This function calculates the expression in reversed polish notation.
+    Function takes list from shunting_yard function.
+    Function returns number - result of calculation.
+
+    I know, I can calculate the expression in the shunting_yard function.
+    I will make it later.
+    """
+
+    if isinstance(expression, str):
+        expression = shunting_yard(expression)
+
+    stack = []
+    right_stack = []
+    for token in expression:
+        if isinstance(token, float) or isinstance(token, int):
+            if len(right_stack) < 0:
+                op1 = stack.pop()
+                op2 = token
+                oper = right_stack.pop()
+                stack.append(OPERATORS[oper].func(op1, op2))
+            else:
+                stack.append(token)
+            continue
+        if token in OPERATORS:
+            op2 = stack.pop()
+            op1 = stack.pop()
+            stack.append(OPERATORS[token].func(op1, op2))
+            continue
+        if token in FUNCTIONS:
+            if FUNCTIONS[token].arg_count == "multiple":
+                count = stack.pop()
+                args = [stack.pop() for _ in range(count)]
+                stack.append(FUNCTIONS[token].func(args))
+            else:
+                stack.append(FUNCTIONS[token].func(stack.pop()))
+            continue
+
+    return stack.pop()
+
+
+if __name__ == "__main__":
+
+    inp = input("Write the expression: ")
+    parsed = parse_expression(inp)
+    print(parsed)
+    polish = shunting_yard(parsed)
+    print(polish)
+    result = calculate(polish)
+    print(result)
