5f98c0781cb6680a0f191e1bf15f8982bb7f86c2
Alexander <mail.0awawa0@gmail.com>
2020-11-13 07:14:14 +0300

    Update crypto_numbers.py

--- scripts/crypto_numbers.py
+++ scripts/crypto_numbers.py
@@ -6,30 +6,48 @@ There is a list of all functions in this module:
 import random
 
 
-# def gcd(a: int, b: int) -> dict:
-#     """Extended Euklid algorithm. Finds greatest common divisor of a and b
-
-#     Args:
-#         a -- first number
-#         b -- second number
-
-#     Return: dictionary with specified keys:
-#         reminder -- greatest common divisor
-#         x, y -- answers to equation ax + by = reminder
-#     """
-#     if b == 0:
-#         return {"x": 1, "y": 0, "reminder": a}
-#     else:
-#         answer = gcd(b, a % b)
-#         x = answer["x"]
-#         y = answer["y"]
-#         d = answer["reminder"]
-#         ret = {"reminder": d, "x": y, "y": x - a // b * y}
-#         return ret
+SMALL_PRIMES = [
+	2,3,5,7,11,13,17,19,23,29,31,37,41,43,47,53,
+    59,61,67,71,73,79,83,89,97,101,103,107,109,
+    113,127,131,137,139,149,151,157,163,167,173,
+    179,181,191,193,197,199,211,223,227,229,233,
+    239,241,251,257,263,269,271,277,281,283,293,
+    307,311,313,317,331,337,347,349,353,359,367,
+    373,379,383,389,397,401,409,419,421,431,433,
+    439,443,449,457,461,463,467,479,487,491,499,
+    503,509,521,523,541,547,557,563,569,571,577,
+    587,593,599,601,607,613,617,619,631,641,643,
+    647,653,659,661,673,677,683,691,701,709,719,
+    727,733,739,743,751,757,761,769,773,787,797,
+    809,811,821,823,827,829,839,853,857,859,863,
+    877,881,883,887,907,911,919,929,937,941,947,
+    953,967,971,977,983,991,997,1009,1013,1019,
+    1021,1031,1033,1039,1049,1051,1061,1063,1069,
+    1087,1091,1093,1097,1103,1109,1117,1123,1129,
+    1151,1153,1163,1171,1181,1187,1193,1201,1213,
+    1217,1223,1229,1231,1237,1249,1259,1277,1279,
+    1283,1289,1291,1297,1301,1303,1307,1319,1321,
+    1327,1361,1367,1373,1381,1399,1409,1423,1427,
+    1429,1433,1439,1447,1451,1453,1459,1471,1481,
+    1483,1487,1489,1493,1499,1511,1523,1531,1543,
+    1549,1553,1559,1567,1571,1579,1583,1597,1601,
+    1607,1609,1613,1619,1621,1627,1637,1657,1663,
+    1667,1669,1693,1697,1699,1709,1721,1723,1733,
+    1741,1747,1753,1759,1777,1783,1787,1789,1801,
+    1811,1823,1831,1847,1861,1867,1871,1873,1877,
+    1879,1889,1901,1907,1913,1931,1933,1949,1951,
+    1973,1979,1987,1993,1997,1999,2003,2011,2017,
+    2027,2029,2039,2053,2063,2069,2081,2083,2087,
+    2089,2099,2111,2113,2129,2131,2137,2141,2143,
+    2153,2161,2179,2203,2207,2213,2221,2237,2239,
+    2243,2251,2267,2269,2273,2281,2287,2293,2297,
+    2309,2311,2333,2339,2341,2347,2351,2357,2371,
+    2377,2381,2383,2389,2393,2399,2411,2417,2423
+]
     
     
 def gcd(n: int, m: int) -> dict:
-    """Extended Euklid algorithm. Finds greatest common divisor of a and b
+    """Extended Euklidean algorithm. Finds greatest common divisor of a and b
 
      Args:
          n -- first number
@@ -128,17 +146,7 @@ def get_prime(n: int) -> int:
         # from 2 to 200
 
         def check_small_primes(n):
-            little_primes = [
-                2, 3, 5, 7, 11, 13, 17, 19,
-                23, 29, 31, 37, 41, 43, 47,
-                53, 59, 61, 67, 71, 73, 79,
-                83, 89, 97, 101, 103, 107,
-                109, 113, 127, 131, 139, 149,
-                151, 157, 163, 167, 173, 179,
-                181, 191, 193, 197, 199
-            ]
-
-            for p in little_primes:
+            for p in SMALL_PRIMES:
                 if n % p == 0 and n != p:
                     return False
             return True
@@ -180,7 +188,7 @@ def prime_factors(n: int) -> list:
     return factors
 
 
-def euler_func(n: int) -> int:
+def eulers_totient(n: int, factors: list = None) -> int:
     """Function counts the positive integers up to a given integer n that are
     relatively prime to n. More about Euler's function:
     https://en.wikipedia.org/wiki/Euler%27s_totient_function
@@ -192,11 +200,28 @@ def euler_func(n: int) -> int:
     """
     if is_prime(n, 10):
         return n - 1
-    n_factors = set(prime_factors(n))
-    result = n
+
+    if factors:
+        mul = 1
+        for f in factors:
+            mul *= f
+
+        if mul != n: 
+            n_factors = set(prime_factors(n))
+        else:
+            n_factors = set(factors)
+    else:
+        factors = prime_factors(n)
+        n_factors = set(factors)
+
+    count = {}
+    for f in n_factors:
+    	count[f] = factors.count(f)
+    result = 1
     for i in n_factors:
-        result *= (1 - 1/i)
-    return round(result)
+        result *= (i ** (count[i] - 1)) * (i - 1)
+
+    return result
 
 
 def primitive_root(n: int, phi_factors: list = None) -> int:
