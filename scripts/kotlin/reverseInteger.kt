be98f5f74419e61a814cb72aac585d041c427cbd
Alexander <mail.0awawa0@gmail.com>
2020-11-18 08:48:17 +0200

    Update reverseInteger.kt

--- scripts/kotlin/reverseInteger.kt
+++ scripts/kotlin/reverseInteger.kt
@@ -2,7 +2,7 @@
 Given a 32-bit signed integer, reverse digits of an integer.
 
 Note:
-Assume we are dealing with an environment that could only store integers within the 32-bit signed integer range: [−231,  231 − 1]. 
+Assume we are dealing with an environment that could only store integers within the 32-bit signed integer range.
 For the purpose of this problem, assume that your function returns 0 when the reversed integer overflows.
 */
 
