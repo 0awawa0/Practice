8c00f6bd0e9c675f51edabd2df5daee6f49348a4
Alexander <mail.0awawa0@gmail.com>
2020-12-10 10:19:21 +0200

    Update inorderTraversal.kt

--- scripts/kotlin/BinaryTree/inorderTraversal.kt
+++ scripts/kotlin/BinaryTree/inorderTraversal.kt
@@ -1,11 +1,11 @@
 
 /*
 Given the root of a binary tree, return the inorder traversal of its nodes' values.
-*/
 
    1
   / \  => [2, 1, 3]
  2   3
+*/
 
 fun inorderTraversal(root: TreeNode?): List<Int> {
     if (root == null) return ArrayList<Int>()
