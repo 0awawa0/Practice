73feb713f60bc97c9f23a447a4aa5a04a3d9698e
Alexander <mail.0awawa0@gmail.com>
2021-08-18 12:55:08 +0300

    Update findKClosesElements.kt

--- scripts/kotlin/findKClosesElements.kt
+++ scripts/kotlin/findKClosesElements.kt
@@ -1,3 +1,13 @@
+/*
+Given a sorted integer array arr, two integers k and x, return the k closest integers to x in the array. The result should also be sorted in ascending order.
+
+An integer a is closer to x than an integer b if:
+
+    |a - x| < |b - x|, or
+    |a - x| == |b - x| and a < b
+
+*/
+
 
 fun findClosestElements(arr: IntArray, k: Int, x: Int): List<Int> {
 	
