46fc69a86781b4ecc024c9a9323a07ee6855ea47
Alexander <mail.0awawa0@gmail.com>
2021-11-17 09:56:17 +0300

    Update countOfUniquePaths.kt

--- scripts/kotlin/countOfUniquePaths.kt
+++ scripts/kotlin/countOfUniquePaths.kt
@@ -1,8 +1,8 @@
 
 /*
-A robot is located at the top-left corner of a m x n grid (marked 'Start' in the diagram below).
+A robot is located at the top-left corner of a m x n grid.
 
-The robot can only move either down or right at any point in time. The robot is trying to reach the bottom-right corner of the grid (marked 'Finish' in the diagram below).
+The robot can only move either down or right at any point in time. The robot is trying to reach the bottom-right corner of the grid.
 
 How many possible unique paths are there?
 */
