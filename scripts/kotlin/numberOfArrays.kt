d739f93999d99fcd9c80e51427ef1d468c6d4d1d
Alexander <mail.0awawa0@gmail.com>
2022-01-23 13:24:02 +0300

    Create numberOfArrays.kt

---
+++ scripts/kotlin/numberOfArrays.kt
@@ -0,0 +1,30 @@
+
+/*
+You are given a 0-indexed array of n integers differences, which describes the differences between each pair of consecutive integers of a hidden sequence of length (n + 1). More formally, call the hidden sequence hidden, then we have that differences[i] = hidden[i + 1] - hidden[i].
+
+You are further given two integers lower and upper that describe the inclusive range of values [lower, upper] that the hidden sequence can contain.
+
+    For example, given differences = [1, -3, 4], lower = 1, upper = 6, the hidden sequence is a sequence of length 4 whose elements are in between 1 and 6 (inclusive).
+        [3, 4, 1, 5] and [4, 5, 2, 6] are possible hidden sequences.
+        [5, 6, 3, 7] is not possible since it contains an element greater than 6.
+        [1, 2, 3, 4] is not possible since the differences are not correct.
+
+Return the number of possible hidden sequences there are. If there are no possible sequences, return 0.
+*/
+
+fun numberOfArrays(differences: IntArray, lower: Int, upper: Int): Int {
+  var lowest = differences[0]
+  var highest = differences[0]
+  var current = 0
+  for (diff in differences) {
+    current += diff
+    highest = maxOf(highest, current)
+    lowest = minOf(lowest, current)
+  }
+        
+  val lowerBound = if (lowest >= 0) lower else maxOf(lower, lower - lowest)
+  val upperBound = if (highest <= 0) upper else minOf(upper, upper - highest)
+        
+  if (lowerBound > upperBound) return 0
+  return upperBound - lowerBound + 1
+}
