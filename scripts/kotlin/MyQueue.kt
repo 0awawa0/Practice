82dcd4bd6fe51b8ba55042567509238698f74bdf
Alexander <mail.0awawa0@gmail.com>
2022-02-28 08:19:27 +0300

    Create MyQueue.kt

---
+++ scripts/kotlin/MyQueue.kt
@@ -0,0 +1,44 @@
+
+/*
+Implement a first in first out (FIFO) queue using only two stacks. The implemented queue should support all the functions of a normal queue (push, peek, pop, and empty).
+
+Implement the MyQueue class:
+
+    void push(int x) Pushes element x to the back of the queue.
+    int pop() Removes the element from the front of the queue and returns it.
+    int peek() Returns the element at the front of the queue.
+    boolean empty() Returns true if the queue is empty, false otherwise.
+
+Notes:
+
+    You must use only standard operations of a stack, which means only push to top, peek/pop from top, size, and is empty operations are valid.
+    Depending on your language, the stack may not be supported natively. You may simulate a stack using a list or deque (double-ended queue) as long as you use only a stack's standard operations.
+
+*/
+
+class MyQueue() {
+    
+    private val firstStack = LinkedList<Int>()
+    private val secondStack = LinkedList<Int>()
+
+    fun push(x: Int) {
+        firstStack.push(x)
+    }
+
+    fun pop(): Int {
+        if (secondStack.isNotEmpty()) return secondStack.pop()
+        while (firstStack.isNotEmpty()) secondStack.push(firstStack.pop())
+        return secondStack.pop()
+    }
+
+    fun peek(): Int {
+        if (secondStack.isNotEmpty()) return secondStack.peek()
+        while (firstStack.isNotEmpty()) secondStack.push(firstStack.pop())
+        return secondStack.peek()
+    }
+
+    fun empty(): Boolean {
+        return firstStack.isEmpty() && secondStack.isEmpty()
+    }
+
+}
