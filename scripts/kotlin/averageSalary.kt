a4c7000b2c124ae43b78862eac3e87a362d3110c
Alexander <mail.0awawa0@gmail.com>
2022-03-02 15:45:29 +0300

    Create averageSalary.kt

---
+++ scripts/kotlin/averageSalary.kt
@@ -0,0 +1,17 @@
+
+/*
+You are given an array of unique integers salary where salary[i] is the salary of the ith employee. Return the average salary of employees excluding the minimum and maximum salary. Answers within 10-5 of the actual answer will be accepted.
+*/
+
+fun average(salary: IntArray): Double {
+  var total = 0.0
+  var maxSalary = salary[0]
+  var minSalary = salary[0]
+  for (s in salary) {
+    total += s
+    maxSalary = maxOf(s, maxSalary)
+    minSalary = minOf(s, minSalary)
+  }
+        
+  return (total - maxSalary.toDouble() - minSalary.toDouble()) / (salary.size - 2)
+}
