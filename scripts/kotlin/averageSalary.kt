20281bdbea6da3482d43c150492a9f949d739ea3
Alexander <mail.0awawa0@gmail.com>
2022-03-02 15:46:53 +0300

    Update averageSalary.kt

--- scripts/kotlin/averageSalary.kt
+++ scripts/kotlin/averageSalary.kt
@@ -1,6 +1,6 @@
 
 /*
-You are given an array of unique integers salary where salary[i] is the salary of the ith employee. Return the average salary of employees excluding the minimum and maximum salary. Answers within 10-5 of the actual answer will be accepted.
+You are given an array of unique integers salary where salary[i] is the salary of the ith employee. Return the average salary of employees excluding the minimum and maximum salary.
 */
 
 fun average(salary: IntArray): Double {
