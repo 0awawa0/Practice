13a59b2002f10092ede915308393e6f17fab6804
Alexander <mail.0awawa0@gmail.com>
2022-03-27 12:34:03 +0300

    Create minDeletion.kt

---
+++ scripts/kotlin/minDeletion.kt
@@ -0,0 +1,31 @@
+
+/*
+You are given a 0-indexed integer array nums. The array nums is beautiful if:
+
+    nums.length is even.
+    nums[i] != nums[i + 1] for all i % 2 == 0.
+
+Note that an empty array is considered beautiful.
+
+You can delete any number of elements from nums. When you delete an element, all the elements to the right of the deleted element will be shifted one unit to the left to fill the gap created and all the elements to the left of the deleted element will remain unchanged.
+
+Return the minimum number of elements to delete from nums to make it beautiful.
+*/
+
+fun minDeletion(nums: IntArray): Int {
+  var i = 0
+  var count = 0
+  while (i < nums.size - 1) {
+    if ((i - count) % 2 == 0 && nums[i] == nums[i + 1]) {
+      var j = i + 1
+      while (j < nums.size && nums[i] == nums[j]) {
+        j++
+        count++
+      }
+      i = j
+    } else i++
+  }
+        
+  if ((nums.size - count) % 2 != 0) count++
+  return count
+}
